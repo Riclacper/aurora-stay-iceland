ac70044a034435ff24f0b5c377ee429962337518
Riclacper <121570392+Riclacper@users.noreply.github.com>
2026-06-29 02:04:03 -0300

    fix: posiciona novas páginas no topo

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
+import ScrollToTop from "../components/ScrollToTop.jsx";
 
 export default function MainLayout() {
   return (
     <div className="app-shell">
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
